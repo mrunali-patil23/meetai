37c5a96f6f96b6b49142f392378eb77872eea5a9
mrunali-patil-23 <you@example.com>
2025-08-17 20:08:33 +0530

    04:authentication ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  const {  data: session } = authClient.useSession() 
+  const { data: session } = authClient.useSession() 
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
