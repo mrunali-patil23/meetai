ea87b8ea6ffd92edc48cc1314825c3de6bc665d0
mrunali-patil-23 <you@example.com>
2025-09-05 18:03:31 +0530

    13: agents filters

--- src/modules/agents/ui/components/columns.tsx
+++ src/modules/agents/ui/components/columns.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { CornerDownRightIcon, VideoIcon } from "lucide-react"
 import { ColumnDef } from "@tanstack/react-table"
+import { CornerDownRightIcon, VideoIcon } from "lucide-react"
 
 import { Badge } from "@/components/ui/badge"
 import { AgentGetOne } from "../../types"
@@ -9,39 +9,39 @@ import { AgentGetOne } from "../../types"
 import { GeneratedAvatar } from "@/components/generated-avatar"
 
 export const columns: ColumnDef<AgentGetOne>[] = [
-  {
-    accessorKey: "name",
-    header: "Agent Name",
-    cell: ({ row }) => (
-        <div className="flex flex-col gap-y-1">
-            <div className="flex items-center gap-x-2">
-                <GeneratedAvatar 
-                    variant="bottsNeutral"
-                    seed={row.original.name}
-                    className="size-6"
-                />
-                <span className="font-semibold capitalize">{row.original.name}</span>
+    {
+        accessorKey: "name",
+        header: "Agent Name",
+        cell: ({ row }) => (
+            <div className="flex flex-col gap-y-1">
+                <div className="flex items-center gap-x-2">
+                    <GeneratedAvatar 
+                        variant="bottsNeutral" 
+                        seed={row.original.name}   
+                        className="size-6"                  
+                    />
+                    <span className="font-semibold capitalizess">{row.original.name}</span>
+                </div>
+                <div className="flex items-center gap-x-2">
+                    <CornerDownRightIcon className="size-3 text-muted-foreground" />
+                    <span className="text-sm text-muted-foreground max-w-[200px] truncate capitalize">
+                        {row.original.instructions}
+                    </span>
+                </div>
             </div>
-            <div className="flex items-center gap-x-2">
-                <CornerDownRightIcon className="size-3 text-muted-foreground" />
-                <span className="text-sm text-muted-foreground max-w-[200px] truncate capitalize">
-                    {row.original.instructions}
-                </span>
-            </div>
-        </div>
-    )
-  },
-  {
-    accessorKey: "meetingCount",
-    header: "Meetings",
-    cell: () => (
-        <Badge
-            variant="outline"
-            className="flex items-center gap-x-2 [&>svg]:size-4"
-        >
-            <VideoIcon className="text-blue-700" />
-            5 Meetings
-        </Badge>
-    )
-  }
+        )
+    },
+    {
+        accessorKey: "meetingCount",
+        header: "Meetings",
+        cell: ({ row }) => (
+            <Badge
+                variant="outline"
+                className="flex items-center gap-x-2 [&>svg]:size-4"
+            >
+                <VideoIcon className="text-blue-700" />
+                {row.original.meetingCount} {row.original.meetingCount === 1 ? "meeting" : "meetings"}
+            </Badge>
+        )
+    }
 ]
